c82bbed99ea7279c99b5b5d832ff606a8aad4b1b
Abhi94496 <133668056+Abhi94496@users.noreply.github.com>
2025-09-29 22:12:00 +0530

    Add files via upload

---
+++ js_practicw/script.js
@@ -0,0 +1,93 @@
+console.log("good_evenings")
+// print numbers from 1 to 10 using a for loop
+// for (let i = 1; i <= 10;  ){
+//     console.log(i)
+//     i++
+// }
+
+
+// print numbers from 10 to 1 using a for loop
+// for( let i = 10; i > 1; i--){
+//     console.log(i)
+// }
+
+// console.log(3%2)
+
+// print odd numbers from 1 to 15 using a while loop
+// let i = 1
+// while(i <= 15){
+//     if ((i % 2 !== 0)){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+
+// print even numbers from 1 to 15 using a while loop
+// let i = 1;
+// while (i <=20){
+//     if (i %2 === 0){console.log(i)};
+//     i++
+// }
+
+// print tabels of 5 from 1 to 10 using a for loop
+// for ( let i = 1; i <=10;){
+//     console.log("5 X", i ,"=", 5*i);
+//     i++
+// }
+
+// print sum of first 100 numbers using a for loop
+// let sum = 0;
+// for(let i = 0; i <= 100; )
+//     {
+//     sum = sum + i;
+//     if (i === 100){console.log(sum)}
+//     i++
+// }
+
+// print numbers from 1 - 50 which are divisible by 3
+// for (let a = 1; a<=50;){
+//     if (a %3 === 0){ console.log(a)};
+//     a++
+// // }
+
+
+
+// 
+// let max = prompt("enter a maxx number")
+// for (let i = 1; i <= max;){
+//     if(i % 2 === 0){
+//         console.log(`${i} is even`);
+//     }
+//     else{console.log(`${i} is odd`)
+//     }
+//     i++
+// }
+
+
+
+
+
+
+// let three = 0
+// let five = 0
+
+// for( let i = 1; i <=100;){
+//     if (i % 3 === 0){
+//         three++
+//     }
+//     else if (i % 5 ===0){
+//         five++
+//     }
+//     i++
+// }
+// console.log(`there are ${three} numbers which are divisible by 3`)
+// console.log(`there are ${five} numbers which are divisible by 5`)
+
+
+// function order(item, quantity, ...extras) {
+//     console.log(`Item: ${item}, Quantity: ${quantity}`);
+//     console.log("Extras:", extras);
+// }
+
+// order("Pizza", 2, "cheese", "olives", "mushrooms");
